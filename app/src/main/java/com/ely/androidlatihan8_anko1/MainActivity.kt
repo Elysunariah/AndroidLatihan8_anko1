bf107fd1581c8f131642ca0da5471406fa01e44d
Elysunariah <ellysunariah255@gmail.com>
2019-07-18 16:58:03 +0700

    Initial commit

---
+++ app/src/main/java/com/ely/androidlatihan8_anko1/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.ely.androidlatihan8_anko1
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.*
+
+class MainActivity : AppCompatActivity(), AnkoLogger{
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_login.setOnClickListener {
+            val username = et_username.text.toString().trim()
+            val pass = et_pass.text.toString().trim()
+            handlingForLogin(username, pass)
+        }
+
+        tv_regis.setOnClickListener {
+            toast("Pindah Ke Halaman Register") //cara membuat toast pada anko
+            startActivity(intentFor<Registrasi>()) //kalo biasanya :
+                                                    //startActivity(Intent(this@MainActivity))
+        }
+    }
+
+    private fun handlingForLogin(uname : String, pass : String){
+        info{"username = $uname, password = $pass "}
+        if (uname.equals("master") && pass.equals("master123")) {
+            toast("Welcome $uname")
+        }else{
+            alert ( title = "Warning", message = "Password atau username salah" ) {
+                positiveButton(buttonText = "OK"){
+                    //do nothing
+                }
+//                  negativeButton(buttonText = "NO"){
+//                      //nanti ngapain
+//                  }
+                isCancelable = false
+            }.show()
+        }
+    }
+}
---
+++ app/src/main/java/com/ely/androidlatihan8_anko1/Registrasi.kt
@@ -0,0 +1,35 @@
+package com.ely.androidlatihan8_anko1
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.register_act.*
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.toast
+
+class Registrasi :AppCompatActivity(), AnkoLogger {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.register_act)
+
+    btn_regis.setOnClickListener {
+        val name = et_inputName.text.toString().trim()
+        val pass = et_inputPassword.text.toString().trim()
+        handlingForRegister(name, pass)
+    }
+}
+private fun handlingForRegister(uname : String, pass : String){
+    when{
+        uname.isEmpty()-> toast("username kosong")
+        pass.isEmpty()->("password kosong")
+        else -> alert (title = "REGISTRASI", message = "User Berhasil Registrasi"){
+            positiveButton(buttonText = "OK"){
+                onBackPressed()
+                finish()
+            }
+            isCancelable = false
+        }.show()
+    }
+}
+}
